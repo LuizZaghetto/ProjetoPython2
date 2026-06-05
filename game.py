a5620e0fb2793891c2d96d2a3a90a57a260dabaa
Luiz Gustavo <luizgustavocampos2005@gmail.com>
2026-06-04 21:17:14 -0300

    Adding travel function and little fixes

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import json 
+import time
 
 def load_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -8,16 +9,55 @@ def load_json(file_path):
     return data_dict
 
 def show_description(data, location):
-    print(data[location]["description"])
+    print("\n" + data[location]["description"] + "\n")
+
+def travel(data, location):
+    exits = data[location]["exits"]
+    
+    if not exits:
+        print("\nNo available paths from here.\n")
+        return location
+        
+    print("\nAvailable ways:")
+    for index, exit_name in enumerate(exits, 1): 
+        print(f"{index} - {exit_name}")
+    print("0 - Cancel travel\n")
+        
+    while True:
+        try:
+            choice = int(input("Choose the new location: "))
+            
+            if choice == 0:
+                return location
+            elif 1 <= choice <= len(exits):
+                return exits[choice - 1]
+            else:
+                print("Error: Choose a valid number from the list.")
+                
+        except ValueError:
+            print("Error: Please enter only integer numbers.")
 
 def gameplay(data, first_loc):
     location = first_loc
+    show_description(data, location)
     while(True):
+        print(f"Actual location: {location}\n")
+
         print("1 - Show actual location description")
-        print("q - Quit the game")
+        print("2 - Travel")
+        print("q - Quit the game\n")
+
         user_input = input("Choose your option: ")
         if user_input == "1": show_description(data, location)
+
+        elif user_input == "2": 
+            location = travel(data, location)
+            show_description(data, location)
+
         elif user_input == "q": return 
+        else: 
+            print("\nPlease input a valid option", flush=True)
+            time.sleep(2)
 
 data = load_json("game.json")
 
