add08f4abb4416c7e4197eb690201892c5697d6d
L1vs <liviamj14@gmail.com>
2026-06-05 13:48:08 -0300

    Added NPC dialogue and relic system

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import json 
 import time
+import os
 
 def load_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -15,17 +16,24 @@ def travel(data, location):
     exits = data[location]["exits"]
     
     if not exits:
-        print("\nNo available paths from here.\n")
+        print('''
+===============================
+No available paths from here.
+''')
         return location
         
-    print("\nAvailable ways:")
+    print('''
+===============================
+Available ways:''')
     for index, exit_name in enumerate(exits, 1): 
         print(f"{index} - {exit_name}")
     print("0 - Cancel travel\n")
         
     while True:
         try:
-            choice = int(input("Choose the new location: "))
+            choice = int(input('''
+===============================
+Choose the new location: '''))
             
             if choice == 0:
                 return location
@@ -37,26 +45,143 @@ def travel(data, location):
         except ValueError:
             print("Error: Please enter only integer numbers.")
 
+def talk_to_npc(data,location,inventory,current_keyword):
+    npc = data[location]["npc"]
+    print(f'''
+===============================
+{npc['name']}:''')
+    print(npc[ 'dialogue'])
+
+    #first NPC (no answer)
+    if "answer" not in npc:
+
+        if npc['item'] not in inventory:
+            inventory.append(npc['item'])
+            current_keyword = npc['keyword']
+
+            print(f'''
+===============================
+You received: {npc['item']}''')
+            print(f'New keyword learned: {npc['keyword']}')
+        else: 
+            print('''
+===============================
+You have already talked to this NPC''')
+
+        return current_keyword
+    #last NPC
+    if npc['name'] == 'Seal Guardian':
+        
+        answer = input('''
+===============================
+Your answer: ''').upper()
+
+        if answer != npc['answer']:
+            print('''
+===============================
+Wrong answer.''')
+            return current_keyword
+        
+        missing = [
+            item for item in npc['required_items']
+            if item not in inventory
+        ]
+
+        if missing:
+            print('''
+===============================
+You don't have all relices yet''')
+            return current_keyword
+        
+        print( '\n', npc['ending'])
+        exit()
+    
+    #normal NPC
+    answer = input('''
+    ===============================
+    Your answer: ''').upper()
+
+    if answer == npc['answer']:
+        if npc['item'] not in inventory:
+            inventory.append(npc['item'])
+            current_keyword = npc['keyword']
+
+            print(f'''
+===============================
+You received: {npc['item']}
+''')
+            print(f"New keyword learned: {npc['keyword']}")
+
+        else: 
+            print('\nYou already have this relic.')
+    
+    else:
+        print('''
+===============================
+Wrong answer.''')
+    return current_keyword
+
+
+
 def gameplay(data, first_loc):
     location = first_loc
-    show_description(data, location)
-    while(True):
-        print(f"Actual location: {location}\n")
+    inventory=[]
+    current_keyword= None
 
+    show_description(data, location)
+
+
+    while(True):
+        print(f'''
+===============================
+Actual location: {location}
+===============================''')
         print("1 - Show actual location description")
         print("2 - Travel")
+        print("3 - Talk to NPC")
+        print("4 - Show inventory")
         print("q - Quit the game\n")
 
         user_input = input("Choose your option: ")
-        if user_input == "1": show_description(data, location)
+        if user_input == "1": 
+            print('===============================')
+            print('''
+Descripition: ''')
+            show_description(data, location)
+            print('===============================')
+
+
 
         elif user_input == "2": 
             location = travel(data, location)
             show_description(data, location)
 
+        elif user_input == '3':
+            current_keyword = talk_to_npc(
+                data,
+                location,
+                inventory,
+                current_keyword
+            )
+        
+        elif user_input == '4':
+            print('''
+===============================
+Inventory:''')
+            
+            if inventory:
+                for item in inventory:
+                    print(f"- {item}")
+            else: 
+                print("Empty")
+
+            print()
+
+
         elif user_input == "q": return 
         else: 
-            print("\nPlease input a valid option", flush=True)
+            print('''===============================
+Please input a valid option''', flush=True)
             time.sleep(2)
 
 data = load_json("game.json")
